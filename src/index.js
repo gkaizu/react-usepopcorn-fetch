63a8fd1f2f5d453d7cac368d116033f71148eb7f
gkaizu <gkaizu90@gmail.com>
2025-11-11 14:03:25 +0900

    Built an interactive StarRating component with customizable ratings, colors, and sizes that displays full/empty stars on hover and click

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
+import { useState } from "react";
 
 import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/*<App />*/}
     <StarRating maxRating={5} />
+    <Test />
   </React.StrictMode>
 );
