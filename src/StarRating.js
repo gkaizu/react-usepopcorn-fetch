44dc44e4ac08e64e8a6d98106b9125f53e94f6af
gkaizu <gkaizu90@gmail.com>
2025-11-11 13:15:55 +0900

    added hover action to the star rating

--- src/StarRating.js
+++ src/StarRating.js
@@ -8,7 +8,6 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
-  gap: "0.4rem",
 };
 
 const textStyle = {
@@ -17,7 +16,8 @@ const textStyle = {
 };
 
 export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(1);
+  const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
@@ -27,18 +27,23 @@ export default function StarRating({ maxRating = 5 }) {
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <Star key={i} onRate={() => handleRating(i + 1)} 
-          full={rating >= i + 1} />
+          <Star
+            key={i}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onRate={() => handleRating(i + 1)}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+          />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span onClick={onRate}>
+    <span onClick={onRate} onMouseEnter={onHoverIn} onMouseLeave={onHoverOut}>
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
